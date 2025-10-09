af361ba3dc45b1a769ecbf907f2049c4d37fc928
Devfiuza <eduardo.costagomes@hotmail.com>
2025-10-09 13:16:56 -0300

    Instação de 'CORS' e ajustes erros.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react"; 
+import { useRef } from "react";
 import api from "../../services/api";
 
 import {
@@ -15,21 +15,18 @@ import {
 import UsersImage from "../../assets/users.png";
 
 function Home() {
-  const inputName= useRef()
-  const inputAge= useRef()
-  const inputEmail=useRef()
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
 
-   async function registerNewUser(){
-    const data =  await api.post('/usuarios', {
-      email:inputEmail.current.value,
-      age:inputAge.current.value,
-      name:inputName.current.value
-      
-    })
-    console.log(data)
+  async function registerNewUser() {
+    await api.post("/usuarios", {
+      email: inputEmail.current.value,
+      age: parseInt(inputAge.current.value),
+      name: inputName.current.value,
+    });
   }
 
-  
   return (
     <Container>
       <TopBackground>
@@ -48,17 +45,27 @@ function Home() {
             <InputLabel>
               Idade<span> *</span>
             </InputLabel>
-            <Input type="number" placeholder="Idade do usuário" ref={inputAge}/>
+            <Input
+              type="number"
+              placeholder="Idade do usuário"
+              ref={inputAge}
+            />
           </div>
         </ContainerInputs>
         <div style={{ width: "100%" }}>
           <InputLabel>
             E-mail<span> *</span>
           </InputLabel>
-          <Input type="email" placeholder="E-mail do usuário" ref={inputEmail}/>
+          <Input
+            type="email"
+            placeholder="E-mail do usuário"
+            ref={inputEmail}
+          />
         </div>
 
-        <Button type="button" onClick={registerNewUser}>Cadastrar usuário</Button>
+        <Button type="button" onClick={registerNewUser}>
+          Cadastrar usuário
+        </Button>
       </Form>
     </Container>
   );
